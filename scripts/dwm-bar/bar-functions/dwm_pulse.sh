fea08c393a9f53f654610de01c956ff7304f5677
Yogesh Lamichane <51807726+YogeshLamichhane@users.noreply.github.com>
2021-09-27 21:31:08 +0545

    Update dwm_pulse.sh

--- scripts/dwm-bar/bar-functions/dwm_pulse.sh
+++ scripts/dwm-bar/bar-functions/dwm_pulse.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 # Dependencies: pamixer
 dwm_pulse () {
-    VOL=$(pamixer --get-volume)
+    TOG=$(pamixer --get-mute)
+    if [ "$TOG" = "true" ]; then
+        printf "MUTE"
+    else
+        VOL=$(pamixer --get-volume)
         if [ "$VOL" = "muted" ] || [ "$VOL" -eq 0 ]; then
             printf "MUTE"
         else
             printf "VOL: %s%%" "$VOL"
         fi
+    fi
     printf "%s\n" "$SEP"
 }
 dwm_pulse
